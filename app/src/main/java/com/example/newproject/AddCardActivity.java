c4753dea5ca591606eb48a5ec72599662725aeb0
dgb30330 <dr.greg.allen.brown@gmail.com>
2021-03-06 17:06:05 -0500

    work for week 4 labs

---
+++ app/src/main/java/com/example/newproject/AddCardActivity.java
@@ -0,0 +1,85 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        String preQ = getIntent().getExtras().getString("preQ");
+        String preA = getIntent().getExtras().getString("preA");
+        String w1 = getIntent().getExtras().getString("wrong1");
+        String w2 = getIntent().getExtras().getString("wrong2");
+        String w3 = getIntent().getExtras().getString("wrong3");
+
+        if(!(preQ.equals("none"))){
+            EditText qEntry = findViewById(R.id.new_question);
+            qEntry.setText(preQ);
+        }
+        if(!(preA.equals("none"))){
+            EditText aEntry = findViewById(R.id.new_answer);
+            aEntry.setText(preA);
+        }
+        if(!(w1.equals("none"))){
+            EditText nw1 = findViewById(R.id.new_wrong1);
+            nw1.setText(w1);
+        }
+        if(!(w2.equals("none"))){
+            EditText nw2 = findViewById(R.id.new_wrong2);
+            nw2.setText(w2);
+        }
+        if(!(w3.equals("none"))){
+            EditText nw3 = findViewById(R.id.new_wrong3);
+            nw3.setText(w3);
+        }
+
+        ImageView cancelIcon = findViewById(R.id.cancel_icon);
+        ImageView saveIcon = findViewById(R.id.save);
+
+
+        cancelIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+        saveIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newQ = ((EditText) findViewById(R.id.new_question)).getText().toString();
+                String newA = ((EditText) findViewById(R.id.new_answer)).getText().toString();
+                String newW1 = ((EditText) findViewById(R.id.new_wrong1)).getText().toString();
+                String newW2 = ((EditText) findViewById(R.id.new_wrong2)).getText().toString();
+                String newW3 = ((EditText) findViewById(R.id.new_wrong3)).getText().toString();
+                if(newA.equals("") || newQ.equals("") || newW1.equals("") || newW2.equals("") || newW3.equals("")){
+                    Toast.makeText(getApplicationContext(), "All fields require text.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Intent changeMain = new Intent(AddCardActivity.this, MainActivity.class);
+                    changeMain.putExtra("newQ", newQ);
+                    changeMain.putExtra("newA", newA);
+                    changeMain.putExtra("newW1", newW1);
+                    changeMain.putExtra("newW2", newW2);
+                    changeMain.putExtra("newW3", newW3);
+                    setResult(RESULT_OK, changeMain);
+                    finish();
+                }
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/newproject/MainActivity.java
+++ app/src/main/java/com/example/newproject/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.newproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +22,15 @@ public class MainActivity extends AppCompatActivity {
         TextView q = findViewById(R.id.flashcard_q);
         TextView a1 = findViewById(R.id.flashcard_a1);
         TextView a2 = findViewById(R.id.flashcard_a2);
+        // a3 is correct answer in start state
         TextView a3 = findViewById(R.id.flashcard_a3);
         TextView a4 = findViewById(R.id.flashcard_a4);
         View parent = findViewById(R.id.parent);
         ImageView icon = findViewById(R.id.visible_icon);
+        ImageView newActivity = findViewById(R.id.new_activity);
+        ImageView editActivity = findViewById(R.id.edit_activity);
+
+        Intent nextActivity = new Intent(MainActivity.this,AddCardActivity.class);
         final boolean[] answerVisible = {false};
 
         a1.setOnClickListener(new View.OnClickListener() {
@@ -74,16 +82,84 @@ public class MainActivity extends AppCompatActivity {
                     answerVisible[0] = false;
                 }
                 else{
+
                     a1.setVisibility(View.VISIBLE);
                     a2.setVisibility(View.VISIBLE);
                     a3.setVisibility(View.VISIBLE);
                     a4.setVisibility(View.VISIBLE);
+
                     icon.setImageResource(R.drawable.visible_no);
                     answerVisible[0] = true;
                 }
 
             }
         });
+        newActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextActivity.putExtra("preQ", "none");
+                nextActivity.putExtra("preA", "none");
+                nextActivity.putExtra("wrong1", "none");
+                nextActivity.putExtra("wrong2", "none");
+                nextActivity.putExtra("wrong3", "none");
+                MainActivity.this.startActivityForResult(nextActivity, 100);
+            }
+        });
+        editActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passQ = q.getText().toString();
+                String passA = a3.getText().toString();
+                String w1 = a1.getText().toString();
+                String w2 = a2.getText().toString();
+                String w3 = a4.getText().toString();
+                nextActivity.putExtra("preQ", passQ);
+                nextActivity.putExtra("preA", passA);
+                nextActivity.putExtra("wrong1", w1);
+                nextActivity.putExtra("wrong2", w2);
+                nextActivity.putExtra("wrong3", w3);
+                MainActivity.this.startActivityForResult(nextActivity, 100);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent changeMain) {
+        super.onActivityResult(requestCode, resultCode, changeMain);
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                //converts new data
+                String newQ = changeMain.getExtras().getString("newQ");
+                String newA = changeMain.getExtras().getString("newA");
+                String newW1 = changeMain.getExtras().getString("newW1");
+                String newW2 = changeMain.getExtras().getString("newW2");
+                String newW3 = changeMain.getExtras().getString("newW3");
+                // gets views for changes
+                TextView q = findViewById(R.id.flashcard_q);
+                TextView a1 = findViewById(R.id.flashcard_a1);
+                TextView a2 = findViewById(R.id.flashcard_a2);
+                TextView a3 = findViewById(R.id.flashcard_a3);
+                TextView a4 = findViewById(R.id.flashcard_a4);
+
+                //changes made
+                q.setText(newQ);
+                q.setTag("change");
+                a3.setText(newA);
+                a1.setText(newW1);
+                a2.setText(newW2);
+                a4.setText(newW3);
+
+                // this just in case answers visible on change.
+                a1.setVisibility(View.INVISIBLE);
+                a2.setVisibility(View.INVISIBLE);
+                a3.setVisibility(View.INVISIBLE);
+                a4.setVisibility(View.INVISIBLE);
+
+                Snackbar.make(findViewById(R.id.parent), "New card created!", Snackbar.LENGTH_INDEFINITE).setDuration(1200).show();
+
+            }
+        }
     }
 
 }
